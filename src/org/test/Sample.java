ee1864a805824cead115a201aee219fc5555a612
Preethi198 <preethiprabhu.2915@gmail.com>
2023-09-29 13:03:25 +0530

    The Project Done

---
+++ src/org/test/Sample.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class Sample {
+	private void StudentId() {
+		System.out.println("Student id : 123");
+		
+	}
+	
+	private void StudentName() {
+		System.out.println("Student name : Preethi");
+		
+	}
+
+}
